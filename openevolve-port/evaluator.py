b8c005a5b1e5025fb83a05bc6aa5363c5db3787b
Ashwin Naren <arihant2math@gmail.com>
2026-02-18 16:44:37 -0800

    decouple openevolve eval completely

--- openevolve-port/evaluator.py
+++ openevolve-port/evaluator.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from functools import partial
 import os
-from pathlib import Path
-import sys
+import random
+from typing import Callable, List, Type
 
 import dspy
 from dspy import Signature
@@ -13,8 +15,95 @@ from litellm import completion
 import bm25s
 import Stemmer
 
-sys.path.append(str(Path(__file__).parent.parent))
-from gepa_artifact.benchmarks.hotpotQA import benchmark as hotpot_b
+from datasets import load_dataset
+
+dataset_size = {"full": None, "lite": 500, "tiny": 200, "test": 50}
+
+
+class Benchmark(ABC):
+    def __init__(self, dataset_mode="lite"):
+        # dataset for training and validation
+        self.dataset = None
+        # dataset for the actual benchmarking
+        self.train_set = None
+        self.test_set = None
+        self.val_set = None
+
+        self.init_dataset()
+        assert self.dataset is not None, "Dataset not initialized"
+        self.max_testset_size = dataset_size[dataset_mode]
+
+        # TODO: FIXME: "test" option is for debugging purposes only, should be removed for final release
+        if dataset_mode == "test":
+            self.dataset = self.trim_dataset(self.dataset, 60)
+            self.create_splits()
+
+        if not self.train_set or not self.test_set or not self.val_set:
+            self.create_splits()
+
+        self.train_set = self.trim_dataset(self.train_set, 150)
+        self.test_set = self.trim_dataset(self.test_set, 300)
+        self.val_set = self.trim_dataset(self.val_set, 300)
+
+        assert self.train_set is not None, "Train set not initialized"
+        assert self.test_set is not None, "Dev set not initialized"
+        assert self.val_set is not None, "Val set not initialized"
+
+    @abstractmethod
+    def init_dataset(self) -> None:
+        """
+        Initializes the dataset for the benchmark, and sets it to self.dataset.
+        Each element in the dataset should be an instance of dspy.Example.
+        """
+        return
+
+    def trim_dataset(self, dataset, size: int) -> None:
+        if size is None or size >= len(dataset):
+            return dataset
+        rng = random.Random()
+        rng.seed(1)
+        return rng.sample(dataset, size)
+
+    def create_splits(self) -> None:
+        """
+        Creates the splits for the dataset (not including test).
+        Upon completion, self.train_set, self.test_set, and self.val_set should be set.
+        """
+
+        total_len = len(self.dataset)
+        self.test_set = self.dataset[: int(0.4 * total_len)]
+        self.val_set = self.dataset[int(0.4 * total_len) : int(0.8 * total_len)]
+        self.train_set = self.dataset[int(0.8 * total_len) :]
+
+    def get_dataset(self):
+        return self.dataset
+
+    def get_train_set(self):
+        return self.train_set
+
+    def get_test_set(self):
+        return self.test_set
+
+
+@dataclass
+class BenchmarkMeta:
+    benchmark: Type[Benchmark]
+    program: List[dspy.Module]
+    metric: Callable
+    dataset_mode: str = "lite"
+    # BenchmarkMeta.num_threads has higher priority than run time argument of num_threads
+    # use this as an upper bound for the number of threads to use
+    num_threads: int = None
+    name: str = None
+    metric_with_feedback: Callable = None
+    feedback_fn_maps: list[dict] = None
+
+class HotpotQABench(Benchmark):
+    def init_dataset(self):
+        raw_datasets = load_dataset("hotpot_qa", "fullwiki", trust_remote_code=True)
+        self.dataset = [
+            dspy.Example(**x).with_inputs("question") for x in raw_datasets["train"]
+        ]
 
 
 class DotDict(dict):
@@ -167,62 +256,17 @@ class HotpotSingleHop(LangProBeDSPyMetaProgram, dspy.Module):
         return dspy.Prediction(answer=final_answer, hop1_docs=hop1_docs)
 
 
-def generate_feedback_prompt(question, log):
-    FEEDBACK_PROMPT_TEMPLATE = """**System behavior overview:**
-- **First hop:** Documents are queried, with only the knowledge of the previous question.
-- **Summary 1:** The agent condenses the information retrieved in the first hop, distilling the most critical information from the top retrieved passages in response to the initial question. 
-- **Second hop:** This query aims to retrieve additional relevant documents not found in the first hop using both the question and the first summary as input.
-- **Summary 2:** Input passages may not always contain every necessary detail, this module should aim to bridge any gaps by inferring or generalizing, drawing upon information from both the initial summary and new passages. It uses the question, first summary, and the second hop's data.
-- **Final Answer:** This module uses the combined information from both summaries to answer the question in an accurate manner.
-
-Provide feedback to improve the following attempt. Your feedback will be used by a prompt optimizing model to improve prompts:
-
-Question: {{question}}
-
-Correct Answer: {{correct_answer}}
-
-First hop output: {{first_hop_output}}
-
-Summary 1 output: {{summary_1_output}}
-
-Second hop output: {{second_hop_output}}
-
-Summary 2 output: {{summary_2_output}}
-
-Final answer: {{final_answer_output}}
-"""
-    return (
-        FEEDBACK_PROMPT_TEMPLATE.replace("{{question}}", log["question"])
-        .replace("{{correct_answer}}", question["answer"])
-        .replace("{{first_hop_output}}", str(log["hop1_output"].passages))
-        .replace("{{summary_1_output}}", str(log["summary_1_output"].summary))
-        .replace("{{second_hop_output}}", str(log["hop2_output"].passages))
-        .replace("{{summary_2_output}}", str(log["summary_2_output"].summary))
-        .replace("{{final_answer_output}}", str(log["final_answer_output"].answer))
-    )
-
-
-def generate_feedback(questions, logs, outputs):
+def generate_feedback(questions, outputs):
     feedbacks = []
     count = 0
-    for question, log, output in zip(questions, logs, outputs):
-        if count > 5:
+    for question, output in zip(questions, outputs):
+        if count > 10:
             break
-        if output[2]:
+        if output[2].score:
             continue
-        count += 1
-        prompt = generate_feedback_prompt(question, log)
-        response = completion(
-            model="openai/gpt-4.1-mini", messages=[{"role": "user", "content": prompt}]
-        )
-        feedbacks.append((log["question"], response.choices[0].message.content))
+        feedbacks.append((question["question"], output[2].feedback))
     CONDENSE_PROMPT = """**System behavior overview:**
-- **First hop:** Documents are queried, with only the knowledge of the previous question.
-- **Summary 1:** The agent condenses the information retrieved in the first hop, distilling the most critical information from the top retrieved passages in response to the initial question. 
-- **Second hop:** This query aims to retrieve additional relevant documents not found in the first hop using both the question and the first summary as input.
-- **Summary 2:** Input passages may not always contain every necessary detail, this module should aim to bridge any gaps by inferring or generalizing, drawing upon information from both the initial summary and new passages. It uses the question, first summary, and the second hop's data.
-- **Final Answer:** This module uses the combined information from both summaries to answer the question in an accurate manner.
-
+This system aims to answer hotpot QA questions in an accurate manner.
 The following has been provided as feedback about the current function of the system. Extract repeating themes and issues and provide a condensed version of the feedback that can be executed on more efficiently by an prompt-optimizing agent.
 """
     condense_prompt = CONDENSE_PROMPT
@@ -255,12 +299,61 @@ adapter = dspy.settings.adapter  # if "qwen" not in lm_name else XMLAdapter()
 dspy.configure(lm=lm_for_optimizer, adapter=adapter)
 
 
+def get_textual_context(d):
+    title_to_sentences = {
+        title: sentences
+        for title, sentences in zip(d["context"]["title"], d["context"]["sentences"])
+    }
+    text = ""
+
+    useful_titles = set(d["supporting_facts"]["title"])
+
+    for title in useful_titles:
+        text += title + " | " + "".join(title_to_sentences[title])
+
+    return text
+
+
+def answer_match_fn(prediction, answers, frac=1.0):
+    """Returns True if the prediction matches any of the answers."""
+    from dspy.dsp.utils import EM, F1
+
+    if frac >= 1.0:
+        return EM(prediction, answers)
+
+    return F1(prediction, answers) >= frac
+
+
+def answer_exact_match_with_feedback(example, pred, trace=None, frac=1.0):
+    ans_match = None
+    if isinstance(example.answer, str):
+        ans_match = answer_match_fn(pred.answer, [example.answer], frac=frac)
+    elif isinstance(example.answer, list):
+        ans_match = answer_match_fn(pred.answer, example.answer, frac=frac)
+
+    textual_context = ""
+    if hasattr(pred, "feedback_text"):
+        textual_context = pred.feedback_text + "\n\n"
+
+    textual_context += get_textual_context(example)
+
+    if ans_match:
+        return dspy.Prediction(
+            score=ans_match,
+            feedback=f"The provided answer, '{pred.answer}' is correct. Here's some additional context behind the answer:\n{textual_context}",
+        )
+    else:
+        return dspy.Prediction(
+            score=ans_match,
+            feedback=f"The provided answer, '{pred.answer}' is incorrect. The correct answer is: {example.answer}. Here's some context behind the answer, and how you could have reasoned to get the correct answer:\n{textual_context}",
+        )
+
+
 def evaluate(prompt_path):
     # Read prompt
     prompt = open(prompt_path, "r").read()
 
-    benchmark_meta = hotpot_b[0]
-    benchmark = benchmark_meta.benchmark()
+    benchmark = HotpotQABench()
     final_eval_set = benchmark.test_set
     # optimizers = get_optimizers()
     # opt_idx = 4  # GEPA
@@ -268,6 +361,7 @@ def evaluate(prompt_path):
     program = HotpotSingleHop(prompt)
     evaluate_prog = dspy.Evaluate(
         devset=final_eval_set,
+        metric=answer_exact_match_with_feedback,
         num_threads=4,
         display_progress=True,
         max_errors=len(final_eval_set) * 10,
@@ -275,7 +369,8 @@ def evaluate(prompt_path):
         return_outputs=True,
     )
     score, outputs = evaluate_prog(program)
-    feedback = generate_feedback(final_eval_set, program.logs, outputs)
+    print("Generating feedback")
+    feedback = generate_feedback(final_eval_set, outputs)
     return EvaluationResult(
         metrics={
             "combined_score": score / 100,
